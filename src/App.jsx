ee32a9e21f8efe11d152edde6dae846962a54d9a
ViveK <vivekn@bravishma.com>
2023-09-09 12:48:41 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import bgImg from "./assets/images/BeyondServices.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="bg-img" src={bgImg} alt="bg-img" />
+    </div>
+  );
+}
+
+export default App;
