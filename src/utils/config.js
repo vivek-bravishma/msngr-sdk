c24f95d49f1bdb0df94f35f30e60002b0799efeb
ViveK <vivekn@bravishma.com>
2023-09-27 12:06:45 +0530

    ui changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export default {
-  loginUrl: "http://localhost:3010/login",
-//   loginUrl: "https://us-central1-nipon-test-350808.cloudfunctions.net/login",
+  // loginUrl: "http://localhost:3010/login",
+  loginUrl: "https://us-central1-nipon-test-350808.cloudfunctions.net/login",
 };
